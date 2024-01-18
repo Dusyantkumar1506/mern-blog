cfea2b91d1732a5a544441f80a42f898073b7bc2
Dusyant1 <dusyant@goldcrownlabs.com>
2024-01-18 21:42:53 +0530

    i craete pages and did a routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import "./App.css";
 
+import Home from "../src/pages/Home";
+import About from "../src/pages/About";
+import SignIn from "../src/pages/SignIn";
+import SignUp from "../src/pages/SignUp";
+import Dashboard from "../src/pages/Dashboard";
+import Projects from "../src/pages/Projects";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
